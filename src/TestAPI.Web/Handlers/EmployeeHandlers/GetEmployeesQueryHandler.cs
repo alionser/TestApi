bcea19d9ef24bcae13dfefe52f5b06b83cc76d6b
alionser <82808235+alionser@users.noreply.github.com>
2022-07-29 01:51:15 +0500

    API: добавил базовую фильтрацию GetEmployees по DepartmentId и Surname

--- src/TestAPI.Web/Handlers/EmployeeHandlers/GetEmployeesQueryHandler.cs
+++ src/TestAPI.Web/Handlers/EmployeeHandlers/GetEmployeesQueryHandler.cs
@@ -17,7 +17,14 @@ public sealed class GetEmployeesQueryHandler : IQueryHandler<GetEmployeesQuery>
 
     public async Task<JsonResult> Handle(GetEmployeesQuery query, CancellationToken ct)
     {
-        var employees = await _dataContext.Employees.ToArrayAsync(ct);
-        return new JsonResult(employees);
+        // var employees = await _dataContext.Employees.ToArrayAsync(ct);
+        var employees = await _dataContext.Employees
+            .Where(query.DepartmentId == null ?
+                e => true : e => e.DepartmentId == query.DepartmentId) //Оно того стоит?
+            .Where(query.Surname == null ?
+                e => true : e => e.Surname == query.Surname)
+            .ToArrayAsync(ct);
+        
+        return new JsonResult(employees); //TODO:  добавить обработку пустого результата
     }
 }
--- src/TestAPI.Web/Queries/GetEmployeesQuery.cs
+++ src/TestAPI.Web/Queries/GetEmployeesQuery.cs
@@ -2,7 +2,8 @@ using TestAPI.Web.Interfaces;
 
 namespace TestAPI.Web.Queries;
 
-public sealed class GetEmployeesQuery : IQuery
+public sealed class GetEmployeesQuery : IQuery //Совпадают Query - параметры запроса и Query - концепция CQS
 {
-    
+    public string Surname { get; set; } //TODO: разобраться с рагистром в запросе
+    public int? DepartmentId { get; set; }
 }
